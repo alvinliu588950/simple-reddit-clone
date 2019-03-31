0ac451b2d24139622d25266c7387edf24e7a0375
LiuChiaHong <ga588950@gmail.com>
2019-03-31 18:09:02 +0800

    refactor: add static file for go lang server

---
+++ client/build/precache-manifest.9b9c7b49c3fcd64ee98593b5135a7798.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4f84619d28a78cef180f",
+    "url": "/static/js/main.39effee6.chunk.js"
+  },
+  {
+    "revision": "92f01a43502c9d249e85",
+    "url": "/static/js/2.1f0fedcb.chunk.js"
+  },
+  {
+    "revision": "4f84619d28a78cef180f",
+    "url": "/static/css/main.77b963a3.chunk.css"
+  },
+  {
+    "revision": "92f01a43502c9d249e85",
+    "url": "/static/css/2.206f2692.chunk.css"
+  },
+  {
+    "revision": "f0f4efc638c4338247e94c651e46e0ac",
+    "url": "/index.html"
+  }
+];
--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/alvinliu588950/simple-reddit-clone/api/topics"
+	"github.com/gin-contrib/static"
 )
 
 func main() {
 	r := gin.Default()
 
+	r.Use(static.Serve("/", static.LocalFile("./client/build", true)))
 	// in-memory storage
 	topicRepo := topics.NewRepo()
 
