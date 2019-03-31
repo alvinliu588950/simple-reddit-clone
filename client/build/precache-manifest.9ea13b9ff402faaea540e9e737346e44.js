7e4f01a448efdce3f9b74f034df62bf32a487a5a
LiuChiaHong <ga588950@gmail.com>
2019-04-01 02:01:14 +0800

    fix: add fronted build #cause heroku only has one build env(only go , no node.)

---
+++ client/build/precache-manifest.9ea13b9ff402faaea540e9e737346e44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "332246f83cac2d4c2bce",
+    "url": "/static/js/main.e80b21b2.chunk.js"
+  },
+  {
+    "revision": "5643857a1017adc5298a",
+    "url": "/static/js/2.c2ea3799.chunk.js"
+  },
+  {
+    "revision": "332246f83cac2d4c2bce",
+    "url": "/static/css/main.da0b7da1.chunk.css"
+  },
+  {
+    "revision": "5643857a1017adc5298a",
+    "url": "/static/css/2.206f2692.chunk.css"
+  },
+  {
+    "revision": "cb039991b62ca1a8f38f63d230391a4c",
+    "url": "/index.html"
+  }
+];
